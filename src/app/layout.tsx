23022d46fc562a0d4f61f05306233dbc5475bf91
Ayush <codesbyayush.gmail.com>
2023-06-30 23:03:31 +0530

    build fail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="shortcut icon" type="image/x-icon" href="./logo.svg" />
+      <link rel="icon" href="/logo.svg" />
       </head>
       <body className={inter.className}>
         <Navbar />
