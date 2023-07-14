0a4d95c0d26b2660fdab0aab47e377b4e156c60c
Ayush <codesbyayush.gmail.com>
2023-07-15 01:57:36 +0530

    netlify build error correction

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -6,7 +6,7 @@ const Popular = async () => {
     const dataRaw = await fetchData("movie/popular",1, 1)
     const data = dataRaw.results
     
-      return  (
+      return  data && (
         <section className="bg-tertiaryGreen bg-opacity-20 custom-scrollbars pb-12">
                 <h2 className="font-semibold text-2xl tracking-wide pb-6 p-8">What's Popular</h2>
             <div className="grid grid-flow-col-dense gap-4 overflow-x-scroll custom-scrollbars px-8">
--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -8,7 +8,7 @@ const Trending = async () => {
     const dataRaw = await fetchData("trending/all/day",0, 1)
     const data = dataRaw.results
     
-      return (
+      return data && (
         <section className="bg-tertiaryGreen bg-opacity-20 ">
                 <h2 className="font-semibold text-2xl tracking-wide pb-6 p-8">Trending</h2>
             <div className="grid grid-flow-col gap-4 overflow-x-scroll custom-scrollbars px-8">
